196f44eff4f9ffcbb4c233b7b190c089dd67505f
Rudranil Goswami <nilrudra068@gmail.com>
2026-09-20 02:34:05 +0530

    coorected CORSmiddleware connection for vercel

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                   "https://frontend-dun-phi-29.vercel.app/",
+                   "https://frontend-dun-phi-29.vercel.app",
                    ],
     allow_credentials=True,
     allow_methods=["*"],
